28039dc41b5aba8a4e17ec6789b410e9541c312c
Hydra1536 <reza15361382@gmail.com>
2026-01-17 23:12:10 +0600

    Initial Commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://127.0.0.1:8000/api",
-    baseURL: "https://tripeld.onrender.com/",
+    baseURL: "https://tripeld.onrender.com/api",
 
 });
 
