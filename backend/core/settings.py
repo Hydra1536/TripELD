8f720f0e1d58cfeae2acfed9c13c7bb11594703e
Hydra1536 <reza15361382@gmail.com>
2026-01-17 22:56:44 +0600

    Initial Commit

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-m199i8izig9b1!8ri^&%i(q3!y_3$2fc8v%a-*)0iwen*+1_ns
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://tripeld.onrender.com/"]
 
 
 # Application definition
